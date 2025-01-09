e6b669b2ddfa3700fe97571bed23a54f04340198
Jack <jacklaing1234@gmail.com>
2025-01-09 22:17:19 +0000

    Added AccountService boilerplate code

--- src/main/java/services/AccountService.java
+++ src/main/java/services/AccountService.java
@@ -1,4 +1,23 @@
 package services;
 
-public class AccountService {
+import models.Account;
+import services.DatabaseService;
+
+public class AccountService{
+
+    // Attributes
+    private Account account;
+
+    // Constructor
+    public AccountService(Account account){
+        this.account = account;
+    }
+
+    // Getters and Setters
+    public Account getAccount(){return account;}
+    public void setAccount(Account account){this.account = account;}
+
+    // Methods
+
+
 }
