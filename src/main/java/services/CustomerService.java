b2cd6918abbad558acd9a25906e1ef48ebd2cabf
Jack <jacklaing1234@gmail.com>
2025-01-11 19:05:04 +0000

    Added base functionality to CustomerService

--- src/main/java/services/CustomerService.java
+++ src/main/java/services/CustomerService.java
@@ -5,4 +5,18 @@ import services.DatabaseService;
 import models.Customer;
 
 public class CustomerService {
+
+    // Attributes
+    private Customer customer;
+    private DatabaseService db = new DatabaseService();
+
+    // Constructor
+    public CustomerService(Customer customer){
+        this.customer = customer;
+    }
+
+    // Getters and Setters
+    public Customer getCustomer(){return customer;}
+    public void setCustomer(Customer customer){this.customer = customer;}
+
 }
