e946c4d16a982f381df7cb7c9b78e515b938b4d7
Jack <jacklaing1234@gmail.com>
2025-01-11 19:15:35 +0000

    Added get and set methods for username and password

--- src/main/java/models/Customer.java
+++ src/main/java/models/Customer.java
@@ -28,4 +28,11 @@ public class Customer {
     public String getLastName(){return lastName;}
     public void setLastName(String lastName){this.lastName = lastName;}
 
+    public String getUsername() {return username;}
+    public void setUsername(String username) {this.username = username;}
+
+    public String getPassword() {return password;}
+    public void setPassword(String password) {this.password = password;}
+
+
 }
