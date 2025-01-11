9e4fbf5ee7bcb6b45492ec9821207ea162fa122e
Jack <jacklaing1234@gmail.com>
2025-01-11 19:23:10 +0000

    TransactionService.createTransaction now adds customer to DB

--- src/main/java/services/TransactionService.java
+++ src/main/java/services/TransactionService.java
@@ -4,6 +4,8 @@ import services.DatabaseService;
 
 import models.Transaction;
 
+import java.sql.SQLException;
+
 public class TransactionService {
 
     // Attributes
@@ -18,4 +20,10 @@ public class TransactionService {
     // Getters and Setters
     public Transaction getTransaction(){return transaction;}
     public void setTransaction(Transaction transaction){this.transaction = transaction;}
+
+    // Methods
+    public void createTransaction() throws SQLException {
+        db.createTransaction(transaction);
+    }
+
 }
