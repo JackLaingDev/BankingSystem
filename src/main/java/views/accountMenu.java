8a4c1753087da9428077dd597daf482942e68741
Jack <jacklaing1234@gmail.com>
2025-01-13 22:07:32 +0000

    Added display balance and transactions

--- src/main/java/views/accountMenu.java
+++ src/main/java/views/accountMenu.java
@@ -1,13 +1,19 @@
 package views;
 
+import models.Transaction;
+
+import java.math.BigDecimal;
+import java.text.DecimalFormat;
+import java.util.List;
 import java.util.Scanner;
 
 public class accountMenu {
 
     private Scanner scanner;
     private String mainMenu = "Please Enter An Option\n" +
-            "1. Login\n" +
-            "2. Create Account";
+            "1. Check Balance\n" +
+            "2. Check Transactions\n" +
+            "3. Close Account";
 
     public accountMenu(Scanner scanner){
         this.scanner = scanner;
@@ -19,6 +25,23 @@ public class accountMenu {
         int choice = scanner.nextInt();
         scanner.nextLine();
         return choice;
+    }
+
+    public void displayBalance(BigDecimal balance){
+        DecimalFormat df = new DecimalFormat("#,###.00");
+        System.out.println("Balance: " + df.format(balance));
+    }
+
+    public void displayTransactions(List<Transaction> transactions){
+        DecimalFormat df = new DecimalFormat("#,###.00");
+
+        for(int i = 0; i < transactions.size(); i++){
+            System.out.printf("%d. Transaction ID: %d, Recipient ID: %d, Amount: £%.2f%n",
+                    i+1,
+                    transactions.get(i).getTransactionID(),
+                    transactions.get(i).getRecipientID(),
+                    transactions.get(i).getAmount());
+        }
     }
 
 }
