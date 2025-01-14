64b5fa57c8337b669304254194428fe99a243f34
Jack <jacklaing1234@gmail.com>
2025-01-14 00:33:10 +0000

    No longer displays closed accounts

--- src/main/java/services/DatabaseService.java
+++ src/main/java/services/DatabaseService.java
@@ -116,7 +116,7 @@ public class DatabaseService {
     public List<Account> getCustAccounts(Customer customer) throws SQLException{
         List<Account> accounts = new ArrayList<>();
 
-        sql = "SELECT * FROM accounts WHERE customerID = ?";
+        sql = "SELECT * FROM accounts WHERE customerID = ? && isClosed = 0";
 
         try(PreparedStatement statement = prepareStatement(sql)){
             statement.setInt(1, customer.getCustomerID());
